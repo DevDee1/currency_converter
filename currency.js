c8071bc7448ba5af6bf6706beee88f4267b85d41
Akinrotimi Dolapo <64320380+DevDee1@users.noreply.github.com>
2025-01-12 13:28:28 +0100

    Update currency.js

--- currency.js
+++ currency.js
@@ -3,30 +3,30 @@ var num1= parseInt(document.querySelector(".naira").value);
 var operation = document.querySelector(".currency").value;
 var compute;
 if (operation == "pou"){
-	compute = num1 / 494.70;
+	compute = num1 /1893.96;
 }	
 else if (operation == "eur"){
-	compute = num1 / 453.46;
+	compute = num1 / 1598.10;
 }else if (operation == "dol"){
-	compute = num1 / 774;
+	compute = num1 /1558.13;
 }else if (operation == "aud"){
-	compute = num1 / 279.25;
+	compute = num1 / 957.31;
 }else if (operation == "nzd"){
-	compute = num1 / 259.00;
+	compute = num1 /866.48;
 }else if (operation == "cad"){
-	compute = num1 / 290.03;
+	compute = num1 /1080.09;
 }else if (operation == "yen"){
-	compute = num1 / 3.66;
+	compute = num1 / 9.84;
 }else if (operation == "rup"){
-	compute = num1 / 5.20;
+	compute = num1 / 18.08;
 }else if (operation == "swi"){
-	compute = num1 / 420.18;
+	compute = num1 / 1700.11;
 }else if (operation == "dir"){
-	compute = num1 / 104.28;
+	compute = num1 /424.21;
 }else if (operation == "sar"){
-	compute = num1 / 23.45;
+	compute = num1 / 81.17;
 }else if (operation == "rur"){
-	compute = num1 / 5.06;
+	compute = num1 / 15.33;
 }
 
 		
